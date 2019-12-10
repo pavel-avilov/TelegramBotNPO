35bd3651bf659cf8bb3b01a9df947ac8f6d655fa
Павел Авилов <avilovpavel6@gmail.com>
2019-12-10 03:19:50 +0300

    Initial Commit

---
+++ bot.py
@@ -0,0 +1,38 @@
+import telebot
+
+import parse
+
+TOKEN = ''
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start', 'go'])
+def start_handler(message):
+    bot.send_message(message.chat.id,
+                     'Привет, я Бот Некомерческих Организация. Если отправите мне ИНН интересующей Вас некомерческой организации, то я дам Вам небольшую справку о ней)')
+
+
+@bot.message_handler(commands=['help'])
+def start_handler(message):
+    bot.send_message(message.chat.id,
+                     'Просто отправите мне ИНН интересующей Вас некомерческой организации)')
+
+@bot.message_handler(content_types=['text'])
+def text_handler(message):
+    text = message.text.lower()
+    chat_id = message.chat.id
+    if len(text) == 10 and text.isdigit():
+        parse.parse(text)
+        message_send = ''
+        if 'fail' in parse.data:
+            bot.send_message(chat_id, 'Организация с таким ИНН не найдена')
+        else:
+            for key, value in parse.data.items():
+                message_send += str(key) + ' - ' + str(value) + '\n'
+            bot.send_message(chat_id, message_send)
+    else:
+        bot.send_message(chat_id, 'Вы ввели некорректный ИНН')
+
+
+bot.polling()
---
+++ parse.py
@@ -0,0 +1,26 @@
+import requests
+import json
+
+data = dict()
+
+TOKEN = ''
+
+def parse(INN):
+    req = requests.get(f"https://api.crftr.net/v1/org/?inn={INN}&apikey={TOKEN}")
+    file = json.loads(req.text)
+    if file['total'] ==1:
+        data['Название организации'] = file['items'][0]['fullName']
+        data['Регион'] = file['items'][0]['regionName'] + ' ' + file['items'][0]['regionCode']
+        data['Тип'] = file['items'][0]['opfType']
+        data['Статус'] = file['items'][0]['egrulStatus']
+        data['ОГРН'] = file['items'][0]['ogrn']
+        data['ИНН'] = file['items'][0]['inn']
+        data['КПП'] = file['items'][0]['kpp']
+        data['Гранты'] = file['items'][0]['incomeDetail']['grants']['totalSum']
+        data['Субсидии'] = file['items'][0]['incomeDetail']['fedSubsidies']['totalSum']
+        data['Контракты'] = int(
+            file['items'][0]['incomeDetail']['contracts44']['totalSum'] + file['items'][0]['incomeDetail']['contracts94'][
+                'totalSum'] + file['items'][0]['incomeDetail']['contracts223']['totalSum'])
+        data['Ссылка'] = 'https://openngo.ru/organization/' + file['items'][0]['ogrn']
+    else:
+        data['fail'] = 0
